7f3a811c2ea8aa9233f4976ee5d030aff7914cb2
Sekwah <contact@sekwah.com>
2022-07-13 22:02:58 +0100

    refactor: update sneak key code

--- src/main/java/com/hrznstudio/matteroverdrive/util/MOKeyHelper.java
+++ src/main/java/com/hrznstudio/matteroverdrive/util/MOKeyHelper.java
@@ -1,18 +1,15 @@
 package com.hrznstudio.matteroverdrive.util;
 
+import com.mojang.blaze3d.platform.InputConstants;
+import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.settings.KeyBinding;
-import net.minecraft.client.util.InputMappings;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
 
 public class MOKeyHelper {
 
-    @OnlyIn(Dist.CLIENT)
     public static boolean isSneakKeyDown() {
-        final KeyBinding keyBindSneak = Minecraft.getInstance().gameSettings.keyBindSneak;
-        final long handle = Minecraft.getInstance().getMainWindow().getHandle();
-        return InputMappings.isKeyDown(handle, keyBindSneak.getKey().getKeyCode());
+        final KeyMapping keyBindSneak = Minecraft.getInstance().options.keyShift;
+        final long handle = Minecraft.getInstance().getWindow().getWindow();
+        return InputConstants.isKeyDown(handle, keyBindSneak.getKey().getValue());
     }
 
 }
