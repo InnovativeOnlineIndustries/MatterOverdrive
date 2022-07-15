5ccd3820457313d0847a037a779a6a1fc8606a35
Sekwah <contact@sekwah.com>
2022-07-15 02:30:35 +0100

    refactor: update RenderAndroidStation

--- src/main/java/com/hrznstudio/matteroverdrive/client/renderer/tile/RenderAndroidStation.java
+++ src/main/java/com/hrznstudio/matteroverdrive/client/renderer/tile/RenderAndroidStation.java
@@ -10,18 +10,21 @@ package com.hrznstudio.matteroverdrive.client.renderer.tile;
 
 import com.hrznstudio.matteroverdrive.MatterOverdrive;
 import com.hrznstudio.matteroverdrive.block.tile.AndroidStationTile;
+import com.hrznstudio.matteroverdrive.reference.ReferenceClient;
 import com.mojang.blaze3d.platform.GlStateManager;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.DefaultVertexFormat;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.mojang.blaze3d.vertex.VertexFormat;
 import com.mojang.math.Vector3f;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.PlayerModel;
+import net.minecraft.client.model.geom.ModelLayers;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderStateShard;
 import net.minecraft.client.renderer.RenderType;
-import net.minecraft.client.renderer.blockentity.BlockEntityRenderDispatcher;
+import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
 
 public class RenderAndroidStation extends RenderStation<AndroidStationTile> {
@@ -38,13 +41,14 @@ public class RenderAndroidStation extends RenderStation<AndroidStationTile> {
                 }))
                 //.alphaState(new RenderStateShard.TransparencyStateShard().AlphaState(0.003921569F))
                 .createCompositeState(true);
-        return RenderType.create("android_station", DefaultVertexFormat.NEW_ENTITY, 7, 256, true, true, state);
+        return RenderType.create("android_station", DefaultVertexFormat.NEW_ENTITY, VertexFormat.Mode.QUADS, 256, true, true, state);
     }
 
-    public static PlayerModel playerModel = new PlayerModel(0.4f, false);
+    public PlayerModel playerModel;
 
-    public RenderAndroidStation(BlockEntityRenderDispatcher rendererDispatcherIn) {
-        super(rendererDispatcherIn);
+    public RenderAndroidStation(BlockEntityRendererProvider.Context rendererContext) {
+        super(rendererContext.getBlockEntityRenderDispatcher());
+        playerModel = new PlayerModel(rendererContext.bakeLayer(ModelLayers.PLAYER), false);
         playerModel.young = false;
     }
 
@@ -60,14 +64,14 @@ public class RenderAndroidStation extends RenderStation<AndroidStationTile> {
             RenderSystem.depthMask(false);
 
 
-            //GlStateManager.color(ReferenceClient.Colors.HOLO.getRed(), ReferenceClient.Colors.HOLO.getGreen(), ReferenceClient.Colors.HOLO.getBlue());
-            //float playerPosX = (float) MathHelper.clampedLerp((float) player.prevPosX, (float) player.getPosX(), partialTicks);
-            //float playerPosZ = (float) MathHelper.clampedLerp((float) player.prevPosZ, (float) player.getPosZ(), partialTicks);
-            //float angle = (float) Math.toDegrees(Math.atan2(playerPosX - (tile.getPos().getX() + 0.5), playerPosZ - (tile.getPos().getZ() + 0.5)) + Math.PI);
+//            RenderSystem.setShaderColor(ReferenceClient.Colors.HOLO.getRed(), ReferenceClient.Colors.HOLO.getGreen(), ReferenceClient.Colors.HOLO.getBlue(), 1.0f);
+//            float playerPosX = (float) Math.clampedLerp((float) player.prevPosX, (float) player.getPosX(), partialTicks);
+//            float playerPosZ = (float) MathHelper.clampedLerp((float) player.prevPosZ, (float) player.getPosZ(), partialTicks);
+//            float angle = (float) Math.toDegrees(Math.atan2(playerPosX - (tile.getPos().getX() + 0.5), playerPosZ - (tile.getPos().getZ() + 0.5)) + Math.PI);
             stack.mulPose(Vector3f.YP.rotationDegrees(180));
             stack.mulPose(Vector3f.YN.rotationDegrees(tile.getLevel().getGameTime() % 360));
             VertexConsumer consumer = bufferIn.getBuffer(ANDROID);
-            float multiply = 0.35f + (tile.getLevel().getGameTime() % (tile.getLevel().random.nextInt(70) + 1) == 0 ? (0.05f * tile.getWorld().rand.nextFloat()) : 0.05f);
+            float multiply = 0.35f + (tile.getLevel().getGameTime() % (tile.getLevel().random.nextInt(70) + 1) == 0 ? (0.05f * tile.getLevel().random.nextFloat()) : 0.05f);
             playerModel.renderToBuffer(stack, consumer ,0, 0, multiply, multiply, multiply, 0.0625F);
 
             RenderSystem.enableCull();
